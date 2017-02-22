7567513e9ee33563d2b18429c2df0321a259d795
Chris Ortman <chris-ortman@uiowa.edu>
2017-02-21 21:57:30 -0600

    Rudimentary hangman

--- src/com/jamescho/game/state/MenuState.java
+++ src/com/jamescho/game/state/MenuState.java
@@ -5,11 +5,20 @@ import com.jamescho.game.main.Resources;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import java.util.Objects;
 
 /**
  * Created by cortman on 2/19/17.
  */
 public class MenuState extends State {
+
+    private Character[] correctLetters = new Character[] { 'D', 'O', 'G'};
+    private int nextLetterIdx = 0;
+    private int wrongGuesses = 0;
+
+    private ArrayList<Character> enteredChars = new ArrayList<>();
+
     @Override
     public void init() {
         System.out.println("Entered MenuState");
@@ -22,8 +31,40 @@ public class MenuState extends State {
 
     @Override
     public void render(Graphics g) {
-        Resources.explosion.loop();
-        g.drawImage(Resources.welcome,0,0,null );
+        g.setColor(Color.red);
+        g.setFont(new Font("Sans Serif", Font.PLAIN, 44));
+
+        if ( enteredChars.size() == 0) {
+            g.drawImage(Resources.welcome, 0, 0, null);
+        } else {
+            String s = "";
+            for(Character c : enteredChars) {
+                s += c;
+            }
+            g.drawString(s, 10, 50);
+        }
+
+        int personStartX = 600;
+        if ( wrongGuesses >= 1) {
+            g.drawOval(personStartX, 75, 40, 40);
+        }
+
+        if (wrongGuesses >= 2) {
+            g.drawLine(personStartX + 20, 75 + 40, personStartX + 20, 95 + 80);
+        }
+
+        if (wrongGuesses >= 3) {
+            g.drawLine(personStartX + 20 - 50, 75 + 40 + 30, personStartX + 20 + 50, 75 + 40 + 30);
+        }
+
+        if (wrongGuesses >= 4) {
+            g.drawLine(personStartX + 20, 95 + 80, personStartX + 20 - 30, 95 + 80 + 70);
+        }
+
+        if (wrongGuesses >= 5) {
+            g.drawLine(personStartX + 20, 95 + 80, personStartX + 20 + 30, 95 + 80 + 70);
+        }
+
     }
 
     @Override
@@ -33,7 +74,18 @@ public class MenuState extends State {
 
     @Override
     public void onKeyPress(KeyEvent e) {
-        System.out.println("Pressed the " + e.getKeyChar() + " key");
+        if (Character.isAlphabetic(e.getKeyChar())) {
+            Character guess = Character.toUpperCase(e.getKeyChar());
+            Character nextLetter = correctLetters[nextLetterIdx];
+
+            if (Objects.equals(guess, nextLetter)) {
+                nextLetterIdx++;
+            } else {
+                wrongGuesses++;
+            }
+
+            enteredChars.add(guess);
+        }
     }
 
     @Override
